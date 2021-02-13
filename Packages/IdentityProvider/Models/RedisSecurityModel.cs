3f7f33f65253bd3679d67e68a2f2b8a852914e94
Jesus Lopez <jelz2yk@hotmail.com>
2021-02-13 16:04:51 +0200

    commit

---
+++ Packages/IdentityProvider/Models/RedisSecurityModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Barracuda.Indentity.Provider.Models
+{
+    public class RedisSecurityModel
+    {
+        public bool Block { get; set; }
+        public List<RefreshTokensModel> Tokens { get; set; }
+    }
+}
