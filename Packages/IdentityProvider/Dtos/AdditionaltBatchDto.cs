7f72072d2397d54a78796f01e741c8286bcb91c6
Jesus Lopez <jelz2yk@hotmail.com>
2021-02-16 16:29:37 +0200

    add file

---
+++ Packages/IdentityProvider/Dtos/AdditionaltBatchDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Barracuda.Indentity.Provider.Dtos
+{
+    public class AdditionaltBatchDto
+    {
+        public List<string> Batch { get; set; }
+
+        public AdditionaltBatchDto()
+        {
+            Batch = Batch != null ? Batch : new List<string>();
+        }
+
+    }
+}
