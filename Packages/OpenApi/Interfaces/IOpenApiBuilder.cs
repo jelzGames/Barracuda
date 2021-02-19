e9bb177b88d8b4c608856eb8dbb687dd8e4ea98c
Jesus Lopez <jelz2yk@hotmail.com>
2021-02-19 16:02:32 +0200

    Add open api

--- Packages/OpenApi/Interfaces/IOpenApiBuilder.cs
+++ Packages/OpenApi/Interfaces/IOpenApiBuilder.cs
@@ -5,7 +5,7 @@ namespace Barracuda.OpenApi.Interfaces
 {
     public interface IOpenApiBuilder
     {
-        public string Build(Assembly assembly, string typeName, HttpRequestMessage request);
+        public string Build(Assembly assembly, string typeName);
         public string Build(Assembly assembly, string typeName, string serverPath);
         public string OpenAPIUI();
         public string OpenAPIAuth();
